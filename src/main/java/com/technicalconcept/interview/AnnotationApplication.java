79861d54ad9cfc32f2ae913e3ab7f080daae1e50
abhishekgangrade <abhishekgangrade17@gmail.com>
2026-07-04 19:08:33 +0530

    Created one faculty class to check the @ComponentScan includes that only with the basePackage and scans and get the data.

--- src/main/java/com/technicalconcept/interview/AnnotationApplication.java
+++ src/main/java/com/technicalconcept/interview/AnnotationApplication.java
@@ -1,18 +1,25 @@
 package com.technicalconcept.interview;
 
+import com.technicalconcept.interview.service.Faculty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan(basePackages = "com.technicalconcept.interview.service;")
 public class AnnotationApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(AnnotationApplication.class, args);
+	}
+
+	@Bean
+	public Faculty faculty(){
+		return  new Faculty();
 	}
 
 }
---
+++ src/main/java/com/technicalconcept/interview/service/Faculty.java
@@ -0,0 +1,12 @@
+package com.technicalconcept.interview.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Faculty {
+
+    public Faculty(){
+
+        System.out.println("Faculty bean created...");
+    }
+}
